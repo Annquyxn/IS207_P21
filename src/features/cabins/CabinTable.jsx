a1536a64311d266e2a6e363cd9f3ac7f70311bc4
Annv11022005 <nguyenvanan0220051@gmail.com>
2025-04-25 14:13:26 +0700

    Initial commit

---
+++ src/features/cabins/CabinTable.jsx
@@ -0,0 +1,86 @@
+// import styled from 'styled-components';
+import CabinRow from 'features/cabins/CabinRow';
+import Spinner from 'ui/Spinner';
+import Table from 'ui/Table';
+import Menus from 'ui/Menus';
+import Empty from 'ui/Empty';
+import { useCabins } from 'features/cabins/useCabins';
+import { useSearchParams } from 'react-router-dom';
+import { Suspense } from 'react';
+
+// v1
+// const TableHeader = styled.header`
+//   display: grid;
+//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//   column-gap: 2.4rem;
+//   align-items: center;
+
+//   background-color: var(--color-grey-50);
+//   border-bottom: 1px solid var(--color-grey-100);
+//   text-transform: uppercase;
+//   letter-spacing: 0.4px;
+//   font-weight: 600;
+//   color: var(--color-grey-600);
+//   padding: 1.6rem 2.4rem;
+// `;
+
+function CabinTable() {
+  const { cabins } = useCabins();
+  const [searchParams] = useSearchParams();
+
+  // if (isLoading) return <Spinner />;
+  // if (!cabins) return <Empty resource={'cabins'} />;
+
+  // 1) Filter
+  const filterValue = searchParams.get('discount') || 'all';
+
+  // This is probably not the most efficient way, but that doesn't matter
+  let filteredCabins;
+  if (filterValue === 'all') filteredCabins = cabins;
+  if (filterValue === 'no-discount')
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  if (filterValue === 'with-discount')
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  // 2) SortBy
+  const sortBy = searchParams.get('sortBy') || 'startDate-asc';
+  const [field, direction] = sortBy.split('-');
+  const modifier = direction === 'asc' ? 1 : -1;
+
+  // This one is better!
+  // .sort((a, b) => a[field].localeCompare(b[field]) * modifier);
+
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
+
+  return (
+    <Menus>
+      <Table columns='9.6rem 0.8fr 2fr 1fr 1fr 3.2rem'>
+        <Table.Header>
+          <div></div>
+          <div>Cabin</div>
+          <div>Capacity</div>
+          <div>Price</div>
+          <div>Discount</div>
+          <div></div>
+        </Table.Header>
+
+        {/* {cabins.map((cabin) => (
+            <CabinRow key={cabin.id} cabin={cabin} />
+          ))} */}
+
+        {/* Render props! */}
+        <Table.Body
+          data={sortedCabins}
+          render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
+        />
+      </Table>
+    </Menus>
+  );
+}
+
+// We could create yet another layer of abstraction on top of this. We could call this component just <Results>, like: Results({data, count, isLoading, columns, rowComponent}). Then <CabinTable> and ALL other tables would simply call that.
+// BUT, creating more abstractions also has a cost! More things to remember, more complex codebase to understand. Sometimes it's okay to just copy and paste instead of creating abstractions
+
+export default CabinTable;
---
+++ src/features/cabins/CreateCabinForm.jsx
@@ -0,0 +1,165 @@
+import { useForm } from 'react-hook-form';
+
+import { useCreateCabin } from 'features/cabins/useCreateCabin';
+import FormRow from 'ui/FormRow';
+import Input from 'ui/Input';
+import Form from 'ui/Form';
+import Button from 'ui/Button';
+import FileInput from 'ui/FileInput';
+import { useEditCabin } from './useEditCabin';
+import { Textarea } from 'ui/Textarea';
+
+function CreateCabinForm({ cabinToEdit, closeModal }) {
+  const { mutate: createCabin, isLoading: isCreating } = useCreateCabin();
+  const { mutate: editCabin, isLoading: isEditing } = useEditCabin();
+  const isWorking = isCreating || isEditing;
+
+  const { id: editId, ...editValues } = cabinToEdit || {};
+  delete editValues.created_at;
+  const isEditSession = Boolean(editId);
+
+  const { register, handleSubmit, formState, reset, getValues } = useForm({
+    defaultValues: isEditSession ? editValues : {},
+  });
+  const { errors } = formState;
+
+  const onSubmit = function (data) {
+    const options = {
+      onSuccess: (data) => {
+        closeModal?.();
+        reset();
+      },
+    };
+
+    const image = typeof data.image === 'object' ? data.image[0] : data.image;
+
+    if (isEditSession)
+      editCabin(
+        {
+          newCabinData: { ...data, image },
+          id: editId,
+        },
+        options
+      );
+    else createCabin({ ...data, image }, options);
+  };
+
+  // Invoked when validation fails
+  const onError = function (errors) {
+    console.log('Failed validation!', errors);
+  };
+
+  // By default, validation happens the moment we submit the form, so when we call handleSubmit. From them on, validation happens on the onChange event [demonstrate]. We cah change that by passing options into useForm ('mode' and 'reValidateMode')
+  // https://react-hook-form.com/api/useform
+
+  // The registered names need to be the same as in the Supabase table. This makes it easier to send the request
+  // "handleSubmit" will validate your inputs before invoking "onSubmit"
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit, onError)} type='modal'>
+      <FormRow label='Cabin name' error={errors?.name?.message}>
+        {/* register your input into the hook by invoking the "register" function */}
+        {/* why the ...? Because this will return an object { onChange, onBlur, customer, ref }, and by spreading we then add all these to the element [show dev tools] */}
+        {/* include validation with required or other standard HTML validation rules: required, min, max, minLength, maxLength, pattern, validate */}
+        {/* errors will return when field validation fails  */}
+        <Input
+          type='text'
+          id='name'
+          disabled={isWorking}
+          {...register('name', { required: 'This field is required' })}
+        />
+      </FormRow>
+
+      <FormRow label='Maximum capacity' error={errors?.maxCapacity?.message}>
+        <Input
+          type='number'
+          id='maxCapacity'
+          disabled={isWorking}
+          {...register('maxCapacity', {
+            required: 'This field is required',
+            min: {
+              value: 1,
+              message: 'Capacity should be at least 1',
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow label='Regular price' error={errors?.regularPrice?.message}>
+        <Input
+          type='number'
+          id='regularPrice'
+          disabled={isWorking}
+          {...register('regularPrice', {
+            required: 'This field is required',
+            min: {
+              value: 1,
+              message: 'Price should be at least 1',
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow label='Discount' error={errors?.discount?.message}>
+        <Input
+          type='number'
+          id='discount'
+          defaultValue={0}
+          disabled={isWorking}
+          {...register('discount', {
+            required: "Can't be empty, make it at least 0",
+            validate: (value) =>
+              getValues().regularPrice >= value ||
+              'Discount should be less than regular price',
+          })}
+        />
+      </FormRow>
+
+      <FormRow
+        label='Description for website'
+        error={errors?.description?.message}
+      >
+        <Textarea
+          type='number'
+          id='description'
+          defaultValue=''
+          disabled={isWorking}
+          {...register('description', { required: 'This field is required' })}
+        />
+      </FormRow>
+
+      <FormRow label='Cabin photo' error={errors?.image?.message}>
+        <FileInput
+          id='image'
+          accept='image/*'
+          disabled={isWorking}
+          {...register('image', {
+            // required: 'This field is required',
+            required: isEditSession ? false : 'This field is required',
+
+            // VIDEO this doesn't work, so never mind about this, it's too much
+            // validate: (value) =>
+            //   value[0]?.type.startsWith('image/') || 'Needs to be an image',
+          })}
+        />
+      </FormRow>
+
+      <FormRow>
+        {/* type is an HTML attribute! */}
+        <Button
+          variation='secondary'
+          type='reset'
+          disabled={isWorking}
+          onClick={() => closeModal?.()}
+        >
+          Cancel
+        </Button>
+        <Button disabled={isWorking}>
+          {isEditSession ? 'Edit cabin' : 'Create new cabin'}
+        </Button>
+      </FormRow>
+    </Form>
+  );
+}
+
+export default CreateCabinForm;
