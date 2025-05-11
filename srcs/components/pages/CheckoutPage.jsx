dcb87e7238ed142a9f9d887d47d5dc24a7a4f1d0
Annv11022005 <nguyenvanan0220051@gmail.com>
2025-05-11 21:29:07 +0700

    route

--- srcs/components/pages/CheckoutPage.jsx
+++ srcs/components/pages/CheckoutPage.jsx
@@ -1,30 +1,34 @@
-import { useState } from 'react'
-import AddressForm from '../components/checkout/AddressForm'
-import PaymentMethods from '../components/checkout/PaymentMethods'
-import OrderSummary from '../components/cart/OrderSummary'
+// import { useState } from 'react'
+// import AddressForm from '../components/checkout/AddressForm'
+// import PaymentMethods from '../components/checkout/PaymentMethods'
+// import OrderSummary from '../components/cart/OrderSummary'
+
 
 function CheckoutPage() {
-  const [selectedMethod, setSelectedMethod] = useState('cod')
+  // const [selectedMethod, setSelectedMethod] = useState('cod')
   
+
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="flex flex-col lg:flex-row gap-8">
-        {/* Left Column */}
+      {/* <div className="flex flex-col lg:flex-row gap-8">
+        Left Column
         <div className="lg:w-2/3">
           <AddressForm />
-          <PaymentMethods 
+          <PaymentMethods
             selectedMethod={selectedMethod}
             setSelectedMethod={setSelectedMethod}
           />
         </div>
-        
-        {/* Right Column */}
+
+        Right Column
         <div className="lg:w-1/3">
           <OrderSummary />
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
 
+
 export default CheckoutPage;
+
--- srcs/components/pages/PaymentPage.jsx
+++ srcs/components/pages/PaymentPage.jsx
@@ -1,37 +1,37 @@
-import { useState } from "react";
-import ShippingInfo from "../shipping/ShippingInfo";
-import PaymentMethods from "@/components/payment/PaymentMethods";
-import PaymentButton from "@/components/payment/PaymentButton";
+// import { useState } from 'react';
+// import ShippingInfo from "../shipping/ShippingInfo";
+// import PaymentMethods from '@/components/payment/PaymentMethods';
+// import PaymentButton from '@/components/payment/PaymentButton';
 
 function PaymentPage() {
-  const [paymentMethod, setPaymentMethod] = useState("cod");
+  //   const [paymentMethod, setPaymentMethod] = useState('cod');
 
-  const order = {
-    customerName: "Nguyễn Văn A",
-    phone: "0000000000",
-    address: "Số ..., Đường..., Phường..., Quận,..., Thành phố",
-    productPrice: 12000000,
-    shippingFee: 0,
-    discount: 500000,
-    discountCode: "GIAMGIA500",
-    total: 11500000,
-  };
+  //   const order = {
+  //     customerName: "Nguyễn Văn A",
+  //     phone: "0000000000",
+  //     address: "Số ..., Đường..., Phường..., Quận,..., Thành phố",
+  //     productPrice: 12000000,
+  //     shippingFee: 0,
+  //     discount: 500000,
+  //     discountCode: "GIAMGIA500",
+  //     total: 11500000
+  //   };
 
-  const handlePayment = () => {
-    console.log("Selected payment method:", paymentMethod);
-    // Xử lý thanh toán ở đây
-  };
+  //   const handlePayment = () => {
+  //     console.log('Selected payment method:', paymentMethod);
+  //     // Xử lý thanh toán ở đây
+  //   };
 
   return (
-    <main className="min-h-screen bg-white p-4">
-      <div className="max-w-[978px] mx-auto">
+    <main className='min-h-screen bg-white p-4'>
+      {/* <div className="max-w-[978px] mx-auto">
         <ShippingInfo order={order} />
         <PaymentMethods
           paymentMethod={paymentMethod}
           setPaymentMethod={setPaymentMethod}
         />
         <PaymentButton onClick={handlePayment} />
-      </div>
+      </div> */}
     </main>
   );
 }
