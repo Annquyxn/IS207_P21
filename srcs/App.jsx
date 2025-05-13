687f33217eb2d845f3c457a36c9d45b9a2069ad7
Annv11022005 <nguyenvanan0220051@gmail.com>
2025-05-13 20:04:55 +0700

    finish user

--- srcs/App.jsx
+++ srcs/App.jsx
@@ -9,38 +9,44 @@ import ShippingInfoPage from './components/pages/ShippingInfoPage';
 import LoginPage from './components/pages/LoginPage';
 import BuildPCPage from './components/pages/BuildPCPage';
 import ProductSelectionPage from './components/pages/ProductSelectionPage';
-import UserUpdate from './components/pages/UserUpdate';
+import UserAddress from './components/features/user/UserAddress';
+import UserOrders from './components/features/user/UserOrders';
+import UserHistory from './components/features/user/UserHistory';
+import UserPage from './components/features/user/UserPage';
+import User from './components/pages/User';
 import Layout from './components/ui/Layout';
+import AccountForm from './components/features/user/FormAccount';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <main className='flex-1'>
-          <Routes>
-            <Route path='/' element={<Layout />}>
-              <Route index element={<Navigate replace to='home' />} />
-              <Route path='home' element={<HomePage />} />
-              <Route path='user' element={<UserUpdate />} />
-              <Route path='login' element={<LoginPage />} />
-              <Route path='san-pham' element={<ProductPage />} />
-              <Route path='thanh-toan' element={<CheckoutPage />} />
-              <Route
-                path='hoan-tat-thanh-toan'
-                element={<CompleteOrderPage />}
-              />
-              <Route path='phuong-thuc-tt' element={<PaymentPage />} />
-              <Route
-                path='thong-tin-giao-hang'
-                element={<ShippingInfoPage />}
-              />
-              <Route path='build-pc' element={<BuildPCPage />} />
-              <Route path='chon-san-pham' element={<ProductSelectionPage />} />
+    <BrowserRouter>
+      <main className='flex-1'>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Navigate replace to='home' />} />
+            <Route path='home' element={<HomePage />} />
+
+            {/* 👉 Nhóm route USER vào đây */}
+            <Route path='user' element={<User />}>
+              <Route index element={<UserPage />} />
+              <Route path='address' element={<UserAddress />} />
+              <Route path='orders' element={<UserOrders />} />
+              <Route path='history' element={<UserHistory />} />
+              <Route path='update' element={<AccountForm />} />
             </Route>
-          </Routes>
-        </main>
-      </BrowserRouter>
-    </>
+
+            <Route path='login' element={<LoginPage />} />
+            <Route path='san-pham' element={<ProductPage />} />
+            <Route path='thanh-toan' element={<CheckoutPage />} />
+            <Route path='hoan-tat-thanh-toan' element={<CompleteOrderPage />} />
+            <Route path='phuong-thuc-tt' element={<PaymentPage />} />
+            <Route path='thong-tin-giao-hang' element={<ShippingInfoPage />} />
+            <Route path='build-pc' element={<BuildPCPage />} />
+            <Route path='chon-san-pham' element={<ProductSelectionPage />} />
+          </Route>
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
