ccb8061d6da54619f75f4209a0264b4b33046c09
pentakll4002 <dangthienan1207@gmail.com>
2025-05-14 09:44:50 +0700

    Cập nhật UI cho CompleteOrderPage.jsx, thêm 1 vài tailwind cho SuccessIcon.jsx và VoucherBadge.jsx

--- srcs/components/features/voucher/VoucherBadge.jsx
+++ srcs/components/features/voucher/VoucherBadge.jsx
@@ -1,30 +1,27 @@
-function VoucherBadge() {
+function VoucherBadge({ code = "GIAMGIA500", color = "#FFC0D3" }) {
   return (
-    <svg
-      className="w-[150px] h-[40px]"
-      viewBox="0 0 150 40"
-      fill="none" 
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect width="150" height="40" rx="5" fill="#FFC0D3" />
-      <path
-        d="M7.50011 19.5C7.50011 23.6421 4.14224 27 0.000104591 27C0.000101328 23.5 3.26355e-06 23.6421 3.26355e-06 19.5C3.26355e-06 15.3579 3.26355e-06 15.5 0.000104591 12C4.14224 12 7.50011 15.3579 7.50011 19.5Z"
-        fill="white"
-      />
-      <text
-        fill="#EA1A1A"
-        xmlSpace="preserve"
-        style={{ whiteSpace: "pre" }}
-        fontFamily="Inter"
-        fontSize="15"
-        fontWeight="bold"
-        letterSpacing="0em"
+    <div className="relative w-[150px] h-[40px]">
+      <svg
+        className="w-full h-full"
+        viewBox="0 0 150 40"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <tspan x="26" y="26.4545">
-          GIAMGIA500
-        </tspan>
-      </text>
-    </svg>
+        {/* Background */}
+        <rect width="150" height="40" rx="5" fill={color} />
+
+        {/* Hình răng cưa bên trái */}
+        <path
+          d="M7.5 19.5C7.5 23.6421 4.14214 27 0 27C0 23.5 0 23.6421 0 19.5C0 15.3579 0 15.5 0 12C4.14214 12 7.5 15.3579 7.5 19.5Z"
+          fill="white"
+        />
+      </svg>
+
+      {/* Text overlay */}
+      <span className="absolute inset-0 flex items-center justify-center font-bold text-[#EA1A1A] text-[15px] font-sans tracking-wide">
+        {code}
+      </span>
+    </div>
   );
 }
 
