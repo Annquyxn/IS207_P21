42c8882f1c0a79f0e699f23e0ece7092c83a0922
pentakll4002 <dangthienan1207@gmail.com>
2025-05-14 20:53:05 +0700

    Tạo file columns, cập nhật UI cho HomePage.jsx

--- srcs/components/pages/HomePage.jsx
+++ srcs/components/pages/HomePage.jsx
@@ -1,8 +1,16 @@
+import LeftColumn from "@/components/features/columns/LeftColumn";
+import CenterColumn from "@/components/features/columns/CenterColumn";
+import RightColumn from "@/components/features/columns/RightColumn";
+
 function HomePage() {
   return (
-    <div>
-      <h1 className='text-sm text-red-500'>Sao deo chay duoc</h1>
-    </div>
+    <main className="bg-white px-4 py-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 px-4">
+        <LeftColumn />
+        <CenterColumn />
+        <RightColumn />
+      </div>
+    </main>
   );
 }
 
--- srcs/components/ui/Footer.jsx
+++ srcs/components/ui/Footer.jsx
@@ -3,7 +3,7 @@ import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
 function Footer() {
   return (
     <footer className="bg-gray-100 text-gray-700 py-12 px-6 text-sm md:text-base">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-5 gap-8">
+      <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
         {/* VỀ CHÚNG TÔI */}
         <section>
           <h3 className="font-semibold text-gray-900 mb-4">VỀ CHÚNG TÔI</h3>
