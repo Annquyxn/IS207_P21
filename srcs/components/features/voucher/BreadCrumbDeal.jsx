0e954b4c107e0390815cce20f3caf3f1560446a3
pentakll4002 <dangthienan1207@gmail.com>
2025-05-15 11:18:51 +0700

    Thêm ShockDeal.jsx vào HomePage.jsx

---
+++ srcs/components/features/voucher/BreadCrumbDeal.jsx
@@ -0,0 +1,15 @@
+function BreadcrumbDeal() {
+  <nav className="flex items-center gap-[21px] px-14 py-[15px] font-['Istok_Web'] font-bold text-base">
+    <a href="/" className="text-[#978787] no-underline">
+      Trang chủ
+    </a>
+    <span className="text-black">Shock Deal</span>
+    <img
+      src="https://cdn.builder.io/api/v1/image/assets/TEMP/00414adea7532e80d93813da310a5d21f4c772ea?placeholderIfAbsent=true&apiKey=59b1ce71d36d483c8e050e8ff471a4c0"
+      alt="Breadcrumb separator"
+      className="w-[5px] h-[8px]"
+    />
+  </nav>;
+}
+
+export default BreadcrumbDeal;
--- srcs/components/pages/HomePage.jsx
+++ srcs/components/pages/HomePage.jsx
@@ -2,19 +2,22 @@ import LeftColumn from "@/components/features/columns/LeftColumn";
 import CenterColumn from "@/components/features/columns/CenterColumn";
 import RightColumn from "@/components/features/columns/RightColumn";
 import ProductPage from "@/components/pages/ProductPage";
+import ShockDeal from "@/components/features/voucher/ShockDeal";
 
 function HomePage() {
   return (
     <main className="bg-white px-4 py-6">
+      <div className="p-5">
+        <ShockDeal />
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 px-4">
         <LeftColumn />
         <CenterColumn />
         <RightColumn />
       </div>
-      
-      {/* ProductSelectionPage nằm bên dưới */}
+
       <div className="mt-6">
-        <ProductPage />  {/* ProductSelectionPage chứa ProductPage */}
+        <ProductPage />
       </div>
     </main>
   );
