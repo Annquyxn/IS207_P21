d43a6f103333e109eeba8cdcaddbc57a39ac4877
pentakll4002 <dangthienan1207@gmail.com>
2025-05-31 12:38:01 +0700

    Cập nhật lại packgage

--- python-ChatBot/qrCode/app/router/health.py
+++ python-ChatBot/qrCode/app/router/health.py
@@ -1,13 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 from datetime import datetime
 from ..schemas import HealthCheckResponse
 from ..utils.qr_generator import generate_mb_qr
 
 router = APIRouter(tags=["Health Check"])
 
+def add_cors_headers(response: Response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    response.headers["Access-Control-Max-Age"] = "3600"
+    return response
+
 @router.get("/", response_model=HealthCheckResponse)
-async def health_check():
+async def health_check(response: Response):
     test_url = generate_mb_qr(1000, "HealthCheck")
+    add_cors_headers(response)
     return {
         "status": "healthy",
         "timestamp": datetime.now().isoformat(),
